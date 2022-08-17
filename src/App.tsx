0a4fc86c59350582fe2fb5e93433064b2f02568b
sjoleee <cent7425@gmail.com>
2022-08-18 00:46:55 +0900

    feat: :zap: 변경되는 arr만 복사하도록 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -31,17 +31,42 @@ const Boards = styled.div`
 function App() {
   const [todo, setTodo] = useRecoilState(TodoState);
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    //기존 로직 : 전체 obj를 다 복사한 후 수정하여 set함수로 대체함.
+    // if (!destination) {
+    //   return;
+    // } else {
+    //   setTodo((prev) => {
+    //     const copiedObj: ITodoState = {};
+    //     Object.keys(prev).map((item) => {
+    //       copiedObj[item] = [...prev[item]];
+    //     });
+    //     const arr = copiedObj[source.droppableId].splice(source.index, 1);
+    //     copiedObj[destination.droppableId].splice(destination.index, 0, ...arr);
+    //     return copiedObj;
+    //   });
+    // }
+
+    //수정된 로직 : obj중에서 변경되는 arr만 복사한 후 수정. 변경되지 않는 arr은 처음부터 복사하지 않는다.
     if (!destination) {
       return;
-    } else {
+    } else if (destination.droppableId === source.droppableId) {
       setTodo((prev) => {
-        const copiedObj: ITodoState = {};
-        Object.keys(prev).map((item) => {
-          copiedObj[item] = [...prev[item]];
-        });
-        const arr = copiedObj[source.droppableId].splice(source.index, 1);
-        copiedObj[destination.droppableId].splice(destination.index, 0, ...arr);
-        return copiedObj;
+        const copiedBoard = [...prev[source.droppableId]];
+        const tmp = copiedBoard.splice(source.index, 1);
+        copiedBoard.splice(destination.index, 0, ...tmp);
+        return { ...prev, [source.droppableId]: copiedBoard };
+      });
+    } else if (destination.droppableId !== source.droppableId) {
+      setTodo((prev) => {
+        const sourceBoard = [...prev[source.droppableId]];
+        const destinationBoard = [...prev[destination.droppableId]];
+        const tmp = sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination.index, 0, ...tmp);
+        return {
+          ...prev,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
+        };
       });
     }
   };
